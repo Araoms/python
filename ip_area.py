b3106c80f027e5d5fea0a3b00054cba5cbf34c90
yubangfan <yubangfan@yeah.net>
2019-11-14 16:05:31 +0800

    Add files via upload

---
+++ ip_area.py
@@ -0,0 +1,81 @@
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+from ipaddress import ip_address
+import os
+import csv
+from datetime import datetime
+from IPy import IP
+# def not_real_ip():
+
+
+ip_list={}
+def is_ping(ips='127.0.0.1', location=''):
+    #首先判断存活性
+    i=0
+    for a_ip in ips:
+
+        while os.system('ping -n 2 -w 1 '+a_ip) == 0:
+            print(a_ip+'is the'+location+'ip_addres')
+            ip_list[loction]=a_ip
+            break
+        if i == 20:
+            break
+        i=i+1
+# try:
+#     from ipaddress import ip_address
+# except ImportError:
+#     from ipaddr import IPAddress as ip_address
+def findIPs(start, end, area):
+    start = ip_address(start)
+    end = ip_address(end)
+    result = []
+    while start <= end:
+            result.append(str(start))
+            start += 1
+    is_ping(result, area)
+
+
+def ip_get(location="BJ"):
+    #如果是网址，可以用这个办法来读取网页
+    print(location)
+    html_doc = 'http://ips.chacuo.net/view/s_'+location
+    req = urllib.request.Request(html_doc)
+    webpage = urllib.request.urlopen(req)
+    html = webpage.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    ips=[]
+    ip_tuple=()
+#web_str = soup.find_all('span',class_='v_r')
+    for l in soup.find_all('dd'):
+        #ips=re.findall('[\d\.]+',str(l))
+        #ips=print(re.findall('[\d\.]+',str(l)))
+        ips.append(re.findall('[\d\.]+',str(l)))
+        for ip in ips:
+            findIPs(ip[0],ip[1], location)
+    print(ips)
+    #print(os.system('ping -n 2 -w 1 110.1.12.1'))
+
+area=['BJ','GD','SD','ZJ','JS','SH','LN','SC','HA','HB','FJ','HN','HE','CQ','SX','JX','SN','AH','HL','GX','JL','YN','TJ','NM',
+      'XJ','GS','GZ','HN','NX','XZ']
+for loction in area:
+    ip_get(loction)
+
+print(ip_list)
+
+
+
+
+# list1 = ['list1_1']
+# list2 = []
+# data = {}
+# for lottery in list1:
+#     data['a1'] = 'a1_1'
+#     data['a2'] = lottery
+#     for skin in ('a3_1', 'a3_2', 'a3_3'):
+#         data['a3'] = skin
+#         for type_ in ('a4_1', 'a4_2'):
+#             data['a4'] = type_
+#             print(data)
+#             list2.append(data)
